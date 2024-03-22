4b12d2bb19c5dc57d3b68eec58c944440a145113
yurahriaziev <yura.hriaziev@gmail.com>
2024-03-22 13:41:23 -0400

    App with homepage frame loading, blank

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print("Hello world!")
+import customtkinter as ctk
+import time
+
+class App(ctk.CTk):
+    def __init__(self, screen_type, user=None):
+        super().__init__()
+        self.width = screen_type[0]
+        self.height = screen_type[1]
+        self.geometry(f'{str(self.width)}x{str(self.height)}')
+
+
+    def boot(self):
+        boot_frame = ctk.CTkFrame(self, fg_color='#c7c7c7', width=self.width, height=self.height).place(x=0, y=0)
+
+        # add code to run the boot animation
+
+        self.homepage(self)
+
+        self.mainloop()
+
+    def homepage(self, parent):
+        home_frame = ctk.CTkFrame(parent, fg_color='#e3e3e3', width=self.width, height=self.height).place(x=0, y=0)
+
+        
+
+if __name__ == "__main__":
+    phone = [400, 700]
+    app = App(phone)
+    app.boot()
+
