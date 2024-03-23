09337a5ee0636b591124a9385dfb635421b5a85d
yurahriaziev <yura.hriaziev@gmail.com>
2024-03-23 12:40:36 -0400

    Added scrollable frame, Opportunity card placeholders

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ class App():
         self.height = screen_type[1]
         self.window.geometry(f'{str(self.width)}x{str(self.height)}')
 
+        self.window.title('HERO')
+
         #fonts
         # self.title_font = ctk.CTkFont()
 
@@ -20,15 +22,24 @@ class App():
         self.window.mainloop()
 
     def homepage(self):
-        self.home_frame = ctk.CTkFrame(self.window, fg_color='#e3e3e3', width=self.width, height=self.height).place(x=0, y=0)
+        self.home_frame = ctk.CTkFrame(self.window, fg_color='#e3e3e3', width=self.width, height=self.height)
+        self.home_frame.place(x=0, y=0)
 
         title_frame = ctk.CTkFrame(self.home_frame,bg_color='#c4c4c4', fg_color='#c4c4c4', width=self.width, height=50).place(x=0, y=0)
         title_text = ctk.CTkLabel(title_frame, text="be a hero", bg_color='#c4c4c4', text_color='#fc5000', font=('Borel', 25, 'bold')).place(x=145, y=10)  # add a font argument to this
 
-        self.main_content_frame = ctk.CTkFrame(self.home_frame, width=self.width, height=self.height-50)
+        self.main_content_frame = ctk.CTkScrollableFrame(self.home_frame, width=self.width-20, height=self.height-60)
         self.main_content_frame.place(x=0,y=50)
 
-        self.frame1 = ctk.CTkFrame(self.main_content_frame, width=self.width-20, height=100, fg_color='#c7c7c7').place(x=10, y=10)
+        for i in range(20):
+            self.card = ctk.CTkFrame(self.main_content_frame, fg_color='#c7c7c7', width=400, height=100)
+            self.card.pack(pady=5)
+
+            self.card_desc = ctk.CTkFrame(self.card, width=270, height=80, fg_color='#e3e3e3')
+            self.card_desc.place(x=100, y=10)
+
+            self.card_img = ctk.CTkFrame(self.card, width=80, height=80)   
+            self.card_img.place(x=10, y=10)    
 
         self.window.mainloop()
 
@@ -38,4 +49,3 @@ if __name__ == "__main__":
     phone = [400, 700]
     app = App(phone)
     app.boot()
-
