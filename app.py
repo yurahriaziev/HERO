6a0bd15a21f200c822d6ab523d01004d2e7ef2fc
yurahriaziev <yura.hriaziev@gmail.com>
2024-03-25 18:23:57 -0400

    Updated card placeholders and added menu button

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+from PIL import Image, ImageTk
+from db import DB
 
 class App():
     def __init__(self, screen_type, user=None):
@@ -6,11 +8,16 @@ class App():
         self.width = screen_type[0]
         self.height = screen_type[1]
         self.window.geometry(f'{str(self.width)}x{str(self.height)}')
+        self.window.resizable(0, 0)
 
         self.window.title('HERO')
 
         #fonts
         # self.title_font = ctk.CTkFont()
+
+        #loading in icons
+        self.menu_icon = ImageTk.PhotoImage(Image.open("icons/menu.png").resize((20, 20)))
+        self.open_card_icon = ImageTk.PhotoImage(Image.open("icons/open_card.png").resize((20, 20)))
 
 
     def boot(self):
@@ -25,21 +32,26 @@ class App():
         self.home_frame = ctk.CTkFrame(self.window, fg_color='#e3e3e3', width=self.width, height=self.height)
         self.home_frame.place(x=0, y=0)
 
-        title_frame = ctk.CTkFrame(self.home_frame,bg_color='#c4c4c4', fg_color='#c4c4c4', width=self.width, height=50).place(x=0, y=0)
-        title_text = ctk.CTkLabel(title_frame, text="be a hero", bg_color='#c4c4c4', text_color='#fc5000', font=('Borel', 25, 'bold')).place(x=145, y=10)  # add a font argument to this
+        title_frame = ctk.CTkFrame(self.home_frame,bg_color='#ff861c', fg_color='#ff861c', width=self.width, height=50).place(x=0, y=0)
+        title_text = ctk.CTkLabel(title_frame, text="be a hero", bg_color='#ff861c', text_color='white', font=('Borel', 25, 'bold')).place(x=145, y=10)  # add a font argument to this
+        menu_button = ctk.CTkButton(title_frame, image=self.menu_icon, text="", width=40, height=40, fg_color='#ff861c', bg_color='#ff861c', corner_radius=2, hover_color='#ff9a42')
+        menu_button.place(x=self.width-45, y=5)
 
-        self.main_content_frame = ctk.CTkScrollableFrame(self.home_frame, width=self.width-20, height=self.height-60)
+        self.main_content_frame = ctk.CTkScrollableFrame(self.home_frame, width=self.width-20, height=self.height-60, fg_color='#dedede')
         self.main_content_frame.place(x=0,y=50)
 
         for i in range(20):
             self.card = ctk.CTkFrame(self.main_content_frame, fg_color='#c7c7c7', width=400, height=100)
             self.card.pack(pady=5)
 
-            self.card_desc = ctk.CTkFrame(self.card, width=270, height=80, fg_color='#e3e3e3')
+            self.card_desc = ctk.CTkFrame(self.card, width=230, height=80, fg_color='#e3e3e3')
             self.card_desc.place(x=100, y=10)
 
             self.card_img = ctk.CTkFrame(self.card, width=80, height=80)   
-            self.card_img.place(x=10, y=10)    
+            self.card_img.place(x=10, y=10)  
+
+            open_card_btn = ctk.CTkButton(self.card, image=self.open_card_icon, text="", width=40, height=80, fg_color='#ff861c', bg_color='#c7c7c7', corner_radius=5, hover_color='#ff9a42')
+            open_card_btn.place(x=340, y=10)
 
         self.window.mainloop()
 
